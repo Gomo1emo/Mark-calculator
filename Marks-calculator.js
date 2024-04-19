66d534b8c674ce53d890ab331bafe26022801ed3
Gomolemo <tshabalalagomolemo@gmail.com>
2024-04-19 02:41:00 +0200

    added for loop

--- Marks-calculator.js
+++ Marks-calculator.js
@@ -15,37 +15,33 @@ function markTotal(x,y,z) {
 }*/
 let status;
 
-let name1 = Abbie;
-let name2 = Thomas;
-let name3 = Lebo;
+//let name1 = Abbie;
+//let name2 = Thomas;
+//let name3 = Lebo;
 
 const prompt = require("prompt-sync")();
-const mark1 = parseFloat(prompt("1st mark"));
-const mark2 = parseFloat(prompt("2nd mark"));
-const mark3 = parseFloat(prompt("3rd mark"));
+var studentName = prompt("student name: ");
+const mark1 = parseFloat(prompt("1st mark: "));
+const mark2 = parseFloat(prompt("2nd mark: "));
+const mark3 = parseFloat(prompt("3rd mark: "));
 
 let total = mark1 + mark2 + mark3;
 let average = total/3;
+studentName.length = 5;
 
 
-    if (average >= 50 && average <= 74){
-        status = 'pass';
+    for (i = 0; i < studentName.length; i++) {
+        if (average >= 50 && average <= 74){
+        status = 'passed!!';
+        }
+        else if (average >= 75 && average <= 100){
+            status = "Got A Distiction!!";
+        }
+        else if (average < 50) {
+            status = "Failed!!";
+        }
+        else if(average > 100) {
+            status = "Check your input";
+        };
+console.log(`${studentName} ${status}`);
     }
-    else if (average >= 75 && average <= 100){
-        status = "You Got A Distiction";
-    }
-    else if (average < 50) {
-        status = "You Fail";
-    }
-    else if(average > 100) {
-        status = "Check your input";
-    };
-console.log(status);
-
-nameArray = ["Abbie","Thomas","Lebo"];
-
-for (i in nameArray) {
-    console.log(status.nameArray.length[0]);
-    console.log(status.nameArray.length[1]);
-    console.log(status.nameArray.length[2]);
-}
